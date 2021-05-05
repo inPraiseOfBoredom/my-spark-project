60b376187f4e1715497deb1132195363aaddde6b
inPraiseOfBoredom <anastazy.andreeva@gmail.com>
2021-05-05 13:40:09 +0000

    started

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="grid place-items-center min-h-screen">
-    <h1 class="text-6xl text-purple-800 font-semibold">Welcome to Spark</h1>
+  <div class="grid place-items-center min-h-screen  bg-gradient-to-b from-red-300 to-gray-400">
+    <h1 class="text-6xl text-pink-50 font-normal underline">Spark</h1>
+    <div class="bg-gray-400 uppercase text-pink-100 p-4 ring-pink-100 ring-4 hover:opacity-80">
+      <button>Click me</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+
 import Vue from "vue";
 
 export default Vue.extend({
